d63006d5ab39838efad93926ad3f2e2a014597c5
PereyraAlexisSebastian <sebapereyra2015epet18@gmail.com>
2026-06-11 20:28:03 -0300

    EJERCICIO FLASK1.v2

--- crud/static/js/main.js
+++ crud/static/js/main.js
@@ -10,33 +10,32 @@ if(btnDelete) {
     });
   })
 }
-//el boton
 const linkTema = document.getElementById('tema-css');
-const botonTema = document.getElementById('btn-tema');
 
-// Las URLs de los temas claro y oscuro
 const urlClaro = "https://bootswatch.com/5/cosmo/bootstrap.min.css";
 const urlOscuro = "https://bootswatch.com/5/darkly/bootstrap.min.css";
 
-// se revisa la memori
 if (localStorage.getItem('tema') === 'oscuro') {
     linkTema.setAttribute('href', urlOscuro);
-    botonTema.textContent = '☀️ Modo Claro';
 } else {
     linkTema.setAttribute('href', urlClaro);
 }
 
-// 4. Escuchamos el clic del botón
-botonTema.addEventListener('click', function() {
-    //donde se cambia el tema 
-    if (linkTema.getAttribute('href') === urlClaro) {
-        linkTema.setAttribute('href', urlOscuro);
-        localStorage.setItem('tema', 'oscuro'); 
-        botonTema.textContent = '☀️ Modo Claro';
-        
-    } else {
-        linkTema.setAttribute('href', urlClaro);
-        localStorage.setItem('tema', 'claro');
-        botonTema.textContent = '🌙 Modo Oscuro';
-    }
+const opcionesTema = document.querySelectorAll('.btn-tema-select');
+
+
+opcionesTema.forEach(opcion => {
+    opcion.addEventListener('click', function(evento) {
+        evento.preventDefault();
+
+        const temaElegido = this.getAttribute('data-tema');
+
+        if (temaElegido === 'oscuro') {
+            linkTema.setAttribute('href', urlOscuro);
+            localStorage.setItem('tema', 'oscuro');
+        } else {
+            linkTema.setAttribute('href', urlClaro);
+            localStorage.setItem('tema', 'claro');
+        }
+    });
 });
